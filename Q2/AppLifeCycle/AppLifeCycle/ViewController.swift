44f97748377b49658b0aba4523d0c9c2b932250b
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-09-18 09:43:57 +0900

    feat: Study about iOS App's Life Cycle

---
+++ Q2/AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by HanGyo Jeong on 2020/09/17.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
